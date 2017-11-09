b16db14db0ddfc49b5581c532a71e11813801c05
Szymon Kunowski <auamarto@users.noreply.github.com>
2017-11-09 13:45:48 +0100

    Update FormConfigResolver.php

--- src/Service/FormConfigResolver.php
+++ src/Service/FormConfigResolver.php
@@ -1,15 +1,1 @@
-<?php
 
-namespace Opstalent\CrudBundle\Service;
-
-use Symfony\Component\HttpFoundation\Request;
-
-/**
- * Class FormConfigResolver
- * @author Szymon Kunowski <szymon.kunowski@gmail.com>
- * @package Opstalent\CrudBundle
- */
-class FormConfigResolver
-{
-    public function resolve(Request)
-}
